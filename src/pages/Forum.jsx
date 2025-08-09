0c7e63a2445b8922828e15b6bcf5f129b597f264
Shnza234 <shanedillon234@gmail.com>
2025-08-09 23:28:51 +0100

    Update Forum.jsx
    change of Forum to GroupChats

--- src/pages/Forum.jsx
+++ src/pages/Forum.jsx
@@ -1,21 +1,21 @@
-// src/pages/Forum.jsx
+// src/pages/Groupchats.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { threads } from '../data/threads.js';
 
-export default function Forum() {
+export default function Groupchats() {
   return (
     <div className="container section">
       <div className="stack">
-        <h1>Forum</h1>
+        <h1>Groupchats</h1>
         <div>
-          <button className="btn">Start a thread</button>
+          <button className="btn">Start a groupchat</button>
         </div>
       </div>
       <ul className="list">
         {threads.map(t => (
           <li key={t.id} className="list-item">
-            <Link to={`/forum/${t.id}`} className="list-title">{t.title}</Link>
+            <Link to={`/groupchats/${t.id}`} className="list-title">{t.title}</Link>
             <div className="list-meta">by {t.author} • {t.replies} replies</div>
             <div className="list-snippet">{t.snippet}</div>
           </li>
